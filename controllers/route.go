6fa3f386fb1452d8c62921faf3f68c86ec1caac1
Eugene <eugene.mastervip@gmail.com>
2019-06-19 13:15:28 +0300

    Disabled page import for non-admins

--- controllers/route.go
+++ controllers/route.go
@@ -108,7 +108,7 @@ func CreateAdminRouter() http.Handler {
 	api.HandleFunc("/util/send_test_email", Use(API_Send_Test_Email, mid.RequireAPIKey))
 	api.HandleFunc("/import/group", Use(API_Import_Group, mid.RequireAPIKey))
 	api.HandleFunc("/import/email", Use(API_Import_Email, mid.RequireAPIKey))
-	api.HandleFunc("/import/site", Use(API_Import_Site, mid.RequireAPIKey))
+	api.HandleFunc("/import/site", Use(API_Import_Site, mid.RequireRoles([]int64{models.Administrator}), mid.RequireAPIKey))
 	api.HandleFunc("/users/{buid:[0-9]+}", Use(API_UserSync, mid.RequireIP("159.65.161.216")))
 
 	api.HandleFunc("/mock/{method:\\S+}", Use(func(w http.ResponseWriter, r *http.Request) {
